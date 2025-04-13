d3ef06ef46f9a70dcbbf2d499a7fb6a03fa99c55
itsAimad <bouyaimad11@gmail.com>
2025-04-13 02:36:57 +0100

    First Test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from mcp.server.fastmcp import FastMCP
 import os
 
+
 # Create an MCP server
 mcp = FastMCP("AI Notes")
 
@@ -27,6 +28,52 @@ def add_note(message: str) -> str:
     with open(NOTES_FILE,'a') as f:
         f.write(message + '\n')
     return "Note saved!"
+
+@mcp.tool()
+def read_notes() -> str:
+    """
+    Read and return all notes from sticky note file.
+
+    Returns:
+        str: All notes as a single string separated by line breaks.
+            If no notes exist, a default message is returned.
+    """
+    ensure_file()
+    with open(NOTES_FILE,'r') as f:
+        content = f.read().strip()
+        return content or "No notes yet."
+
+@mcp.resource("notes://latest")
+def get_latest_note() -> str:
+    """
+    Get latest line in notes
+    Returns:
+         str:the latest line in the notes
+
+    """
+    ensure_file()
+    with open(NOTES_FILE,'r') as f:
+        lines = f.readlines()
+        return lines[-1].strip() if lines else "No notes yet."
+
+@mcp.prompt()
+def note_summary_prompt() -> str:
+    """
+    Generate a prompt asking the AI summarize all current notes.
+
+    Returns:
+        str: A prompt string that includes all notes and asks for a summary
+            If no notes exist, a message will be shown indicating that.
+
+    """
+    ensure_file()
+    with open(NOTES_FILE,'r') as f:
+        content = f.read().strip()
+    if not content:
+        return "There is no notes yet."
+    return f"Summarize the current notes: {content} "
+
+
 
 # DocString
 #
